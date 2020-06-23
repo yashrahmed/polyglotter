683d53b05e1ddfcc315b9e3dc669e0212d214216
yashrahmed <yashrahmed@gmail.com>
2020-06-22 22:27:53 -0500

    [haskell] - Chapter 6: Modules and module examples.

---
+++ haskell/Areas/Circle.hs
@@ -0,0 +1,7 @@
+module Areas.Circle(
+    area
+) 
+where
+
+area :: (Floating a) => a -> a
+area x = pi * ((x * x))
---
+++ haskell/areas.hs
@@ -0,0 +1,15 @@
+import qualified Areas.Circle as Circle
+import qualified Areas.Square as Square
+
+
+-- module Areas(
+--     circleArea,
+--     squareArea
+-- ) 
+-- where
+
+-- squareArea :: (Num a) => a -> a
+-- squareArea x = x * x
+
+-- circleArea :: (Floating a) => a -> a
+-- circleArea x = pi * ((x * x))
