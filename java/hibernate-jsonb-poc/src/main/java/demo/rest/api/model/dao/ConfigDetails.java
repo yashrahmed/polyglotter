c023bb29c5879c231b24f5e119faaf756ff71f73
yashrahmed <yashrajjayaraj.digge@here.com>
2020-12-07 04:48:54 -0600

    [Java - Spring] Sample spring boot project to demonstrate jsonb querying with spring data

---
+++ java/hibernate-jsonb-poc/src/main/java/demo/rest/api/model/dao/ConfigDetails.java
@@ -0,0 +1,22 @@
+package demo.rest.api.model.dao;
+
+public class ConfigDetails {
+	private String uuid;
+	private String code;
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+}
---
+++ java/hibernate-jsonb-poc/src/main/java/demo/rest/api/model/dto/ConfigResultDto.java
@@ -0,0 +1,7 @@
+package demo.rest.api.model.dto;
+
+public interface ConfigResultDto {
+	Integer getId();
+	String getUuid();
+	String getCode();
+}
